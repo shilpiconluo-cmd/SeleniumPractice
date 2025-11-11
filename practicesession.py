41b91509d9cd2e8c99512bc1e74528962fe101b8
shilpiconluo-cmd <shilpi.conluo@gmail.com>
2025-11-11 17:38:04 +0100

    parctice

--- practicesession.py
+++ practicesession.py
@@ -33,6 +33,10 @@ class Practice():
         drop1 = driver.find_element(By.ID,"page-menu")
         dd = Select(drop1)
         dd.select_by_value("10")
+        driver.find_element(By.XPATH,"//button[contains(@class, 'react-date-picker__calendar-button')]").click()
+        #time.sleep(4)
+        date = WebDriverWait(driver,10).until(EC.element_to_be_clickable((By.XPATH,"//button[.//abbr[contains(@aria-label, '14') and contains(@aria-label, 'November 2025')]]")))
+        date.click()
         time.sleep(4)
 
 
