96bdca1571b4403979719bf00518162ae28d54e0
shilpiconluo-cmd <shilpi.conluo@gmail.com>
2025-11-12 13:35:31 +0100

    practice more

--- practice2.py
+++ practice2.py
@@ -22,13 +22,31 @@ class Flightdeals():
         wait = WebDriverWait(driver, 10)
         driver.find_element(By.ID, "ctl00_mainContent_ddl_originStation1_CTXT").click()
         time.sleep(4)
-        origin =  wait.until(EC.element_to_be_clickable((By.XPATH, "//a[@value='JAI']")))
-        origin.click()
-        
+        origin, destination = Flightdeals.readfile()
 
+        driver.find_element(By.ID, "ctl00_mainContent_ddl_originStation1_CTXT").click()
+        time.sleep(1)  # wait for dropdown
+        origin_element = wait.until(EC.element_to_be_clickable(
+            (By.XPATH, f"//div[@class='left1']//a[@value='{origin}']")  # scope to origin div
+        ))
+        origin_element.click()
+        #driver.execute_script("arguments[0].click();", origin_element)
 
+        # Select destination
+        driver.find_element(By.ID, "ctl00_mainContent_ddl_destinationStation1_CTXT").click()
+        time.sleep(1)  # wait for dropdown
+        destination_element = wait.until(EC.element_to_be_clickable(
+            (By.XPATH, f"//div[@class='right1']//a[@value='{destination}']")  # scope to destination div
+        ))
+        destination_element.click()
+        #driver.execute_script("arguments[0].click();", destination_element)
 
-
+    @staticmethod
+    def readfile():
+        with open("Citiesfor flight.txt", "r") as f:
+         line = f.readline().strip()
+         origin_element, destination_element = line.split(",")
+         return origin_element, destination_element
 
 
 
