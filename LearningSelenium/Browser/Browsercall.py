1d6355af89a5790fd657dbf0806224903068df80
shilpiconluo-cmd <shilpi.conluo@gmail.com>
2025-11-14 13:15:35 +0100

    practice

--- LearningSelenium/Browser/Browsercall.py
+++ LearningSelenium/Browser/Browsercall.py
@@ -1,9 +1,8 @@
 from selenium import webdriver
-from selenium.webdriver.support.ui import WebDriverWait
 
 
 driver = webdriver.Chrome()
-wait = WebDriverWait(driver,10)
+
 class openurl():
     def pageopen(self):       #Every method inside a class takes self as the first argument — it refers to the current object of the class.
 
--- LearningSelenium/TestCases/Testcases.py
+++ LearningSelenium/TestCases/Testcases.py
@@ -1,41 +1,44 @@
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support import expected_conditions as EC
+from LearningSelenium.Browser.Browsercall import openurl
+from LearningSelenium.Browser.XPath import searchbox, text1, text2, text3,C,A,B
 import time
+
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium import webdriver
 
 import os    # to delete files
 
-from LearningSelenium.Browser import Browsercall
-
-
 class testcase():
     def __init__(self): # constructor — it automatically runs when you create an object of this class.
-        self.browser = Browsercall.openurl().pageopen() # stores the returned driver (browser) inside self.browser.
+        self.browser = openurl().pageopen() # stores the returned driver (browser) inside self.browser.
     def tc1(self):
         print(self.browser.title)
-        Browsercall.driver.find_element(By.CLASS_NAME, "search-keyword").send_keys("ber")
-        text1 = Browsercall.driver.find_element(By.XPATH, "//h4[text()='Cucumber - 1 Kg']").text
-        text2=  Browsercall.wait.until(EC.visibility_of_element_located((By.XPATH, "//h4[text()='Raspberry - 1/4 Kg']"))).text
-        text3 = Browsercall.wait.until(EC.visibility_of_element_located((By.XPATH, "//h4[text()='Strawberry - 1/4 Kg']"))).text
-        text =  [text1, text2, text3]   # list of all texts
+        self.browser.find_element(*searchbox).send_keys("ber")
+        T1= self.browser.find_element(*text1).text
+        T2= WebDriverWait(self.browser,10).until(EC.visibility_of_element_located(text2)).text
+        T3= WebDriverWait(self.browser,10).until(EC.visibility_of_element_located(text3)).text
+
+        text =  [T1, T2, T3]   # list of all texts
         file_name = "fruits.txt"   #genereta file
         with open("fruits.txt","w") as file:
             for item in text:
                 file.write(item +"\n")
         time.sleep(4)
-        C= Browsercall.wait.until(EC.element_to_be_clickable((By.XPATH, "//div[@class='products']//div[1]//div[3]//button[1]")))
-        C.click()
-        A= Browsercall.wait.until(EC.element_to_be_clickable((By.XPATH, "//div[@class='products']//div[2]//div[3]//button[1]")))
-        A.click()
-        B= Browsercall.wait.until(EC.element_to_be_clickable((By.XPATH, "//div[@class='products']//div[3]//div[3]//button[1]")))
-        B.click()
-        cart= Browsercall.wait.until(EC.element_to_be_clickable((By.XPATH, "//a[@class='cart-icon']")))
+
+        WebDriverWait(self.browser,10).until(EC.element_to_be_clickable(C)).click()
+        WebDriverWait(self.browser, 10).until(EC.element_to_be_clickable(A)).click()
+        WebDriverWait(self.browser, 10).until(EC.element_to_be_clickable(B)).click()
+        cart =  WebDriverWait(self.browser,10).until(EC.element_to_be_clickable((By.XPATH, "//a[@class='cart-icon']")))
         cart.click()
-        button= Browsercall.wait.until(EC.element_to_be_clickable((By.XPATH, "//*[text()='PROCEED TO CHECKOUT']")))
+        button =  WebDriverWait(self.browser,10).until(EC.element_to_be_clickable((By.XPATH, "//*[text()='PROCEED TO CHECKOUT']")))
         button.click()
 
-        text4 = Browsercall.wait.until(EC.visibility_of_element_located((By.XPATH, "//*[text()='Cucumber - 1 Kg']"))).text
-        text5 = Browsercall.wait.until(EC.visibility_of_element_located((By.XPATH, "//*[text()='Raspberry - 1/4 Kg']"))).text
-        text6 = Browsercall.wait.until(EC.visibility_of_element_located((By.XPATH, "//*[text()='Strawberry - 1/4 Kg']"))).text
+
+        text4 = WebDriverWait(self.browser,10).until(EC.visibility_of_element_located((By.XPATH, "//*[text()='Cucumber - 1 Kg']"))).text
+        text5 = WebDriverWait(self.browser,10).until(EC.visibility_of_element_located((By.XPATH, "//*[text()='Raspberry - 1/4 Kg']"))).text
+        text6 = WebDriverWait(self.browser,10).until(EC.visibility_of_element_located((By.XPATH, "//*[text()='Strawberry - 1/4 Kg']"))).text
+
 
         content = (text4, text5, text6)
         file_name = "shopping.txt"
